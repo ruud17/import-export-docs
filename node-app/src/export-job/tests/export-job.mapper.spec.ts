a6524b5fe17f9c4ae9d44ae9ddf325dc88b8a687
Mehmed Jusic <mehmedjusic@gmail.com>
2023-10-20 23:43:42 +0200

    Added more unit tests

--- node-app/src/export-job/tests/export-job.mapper.spec.ts
+++ node-app/src/export-job/tests/export-job.mapper.spec.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExportJobMapperService } from '../mappers/export-job-mapper.service';
+import { ExportJob } from '../schema/export-job.schema';
+import { JobState } from '../../common/enums/state.enum';
 
 describe('ExportJobMapperService', () => {
   let service: ExportJobMapperService;
@@ -16,5 +19,65 @@ describe('ExportJobMapperService', () => {
     expect(service).toBeDefined();
   });
 
-  // Add more tests for mapper methods...
+  describe('entityToDto', () => {
+    it('should map ExportJob entity to CreateExportJobResponseDto', () => {
+      const mockEntity: any = {
+        bookId: '123',
+        type: 'epub',
+        state: 'pending',
+        created_at: new Date('2023-10-20T20:00:00.000Z'),
+        updated_at: new Date('2023-10-20T20:10:00.000Z'),
+      };
+      const expectedDto = {
+        bookId: '123',
+        type: 'epub',
+        state: 'pending',
+        created_at: new Date('2023-10-20T20:00:00.000Z'),
+        updated_at: new Date('2023-10-20T20:10:00.000Z'),
+      };
+      const result = service.entityToDto(mockEntity);
+      expect(result).toEqual(expectedDto);
+    });
+  });
+
+  describe('groupEntitesByStateToDto', () => {
+    it('should group ExportJob entities by their state', () => {
+      const mockEntities: ExportJob[] = [
+        {
+          state: JobState.Pending,
+          bookId: '111',
+          type: 'word',
+          created_at: '2023-10-20T20:28:37.643Z',
+          updated_at: '2023-10-20T20:28:37.643Z',
+        },
+        {
+          state: JobState.Finished,
+          bookId: '222',
+          type: 'word',
+          created_at: '2023-10-20T20:28:37.643Z',
+          updated_at: '2023-10-20T20:28:37.643Z',
+        },
+        {
+          state: JobState.Pending,
+          bookId: '333',
+          type: 'word',
+          created_at: '2023-10-20T20:28:37.643Z',
+          updated_at: '2023-10-20T20:28:37.643Z',
+        },
+      ] as any[];
+
+      const removeState = (arr) => arr.map(({ state, ...rest }) => rest);
+      const expectedDto = {
+        [JobState.Pending]: [mockEntities[0], mockEntities[2]],
+        [JobState.Finished]: [mockEntities[1]],
+      };
+      const expectedDtoWithoutState = {
+        [JobState.Pending]: removeState(expectedDto[JobState.Pending]),
+        [JobState.Finished]: removeState(expectedDto[JobState.Finished]),
+      };
+
+      const result = service.groupEntitesByStateToDto(mockEntities);
+      expect(result).toEqual(expectedDtoWithoutState);
+    });
+  });
 });
--- node-app/src/import-job/tests/import-job.service.spec.ts
+++ node-app/src/import-job/tests/import-job.service.spec.ts
@@ -4,12 +4,21 @@ import { ImportJobMapperService } from '../mapper/import-job-mapper.service';
 
 describe('ImportJobService', () => {
   let service: ImportJobService;
+  let mockMapper;
+  let mockDocument;
+  let mockModel;
 
   beforeEach(async () => {
-    const mockModel = {
-      // Mock the mongoose Model methods if needed...
+    mockDocument = {
+      save: jest.fn(),
     };
-    const mockMapperService = {
+
+    mockModel = jest.fn().mockReturnValue(mockDocument); // this will make it act like a constructor
+    mockModel.save = jest.fn();
+    mockModel.find = jest.fn().mockReturnThis();
+    mockModel.exec = jest.fn();
+
+    mockMapper = {
       entityToDto: jest.fn(),
       groupEntitesByStateToDto: jest.fn(),
     };
@@ -23,7 +32,7 @@ describe('ImportJobService', () => {
         },
         {
           provide: ImportJobMapperService,
-          useValue: mockMapperService,
+          useValue: mockMapper,
         },
       ],
     }).compile();
@@ -31,9 +40,58 @@ describe('ImportJobService', () => {
     service = module.get<ImportJobService>(ImportJobService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  // Additional tests for service methods can be added here...
+  it('should retrieve all import jobs', async () => {
+    const mockJobs = [
+      {
+        bookId: '888',
+        type: 'word',
+        state: 'pending',
+        url: 'http://book.com',
+        created_at: '2023-10-20T20:28:37.643Z',
+      },
+      {
+        bookId: '3333',
+        type: 'evernote',
+        state: 'finished',
+        url: 'http://book.com',
+        created_at: '2023-10-20T19:14:45.438Z',
+        updated_at: '2023-10-20T19:15:45.423Z',
+      },
+    ];
+    const mockGroupedJobsResponse = {
+      pending: [
+        {
+          bookId: '888',
+          type: 'word',
+          url: 'http://book.com',
+          created_at: '2023-10-20T20:28:37.643Z',
+        },
+      ],
+      finished: [
+        {
+          bookId: '3333',
+          type: 'evernote',
+          url: 'http://book.com',
+          created_at: '2023-10-20T19:14:45.438Z',
+          updated_at: '2023-10-20T19:15:45.423Z',
+        },
+      ],
+    };
+
+    mockModel.find.mockReturnThis();
+    mockModel.exec.mockResolvedValueOnce(mockJobs);
+    mockMapper.groupEntitesByStateToDto.mockReturnValue(
+      mockGroupedJobsResponse,
+    );
+
+    const result = await service.findAll();
+
+    expect(mockModel.find).toHaveBeenCalled();
+    expect(mockMapper.groupEntitesByStateToDto).toHaveBeenCalledWith(mockJobs);
+    expect(result).toBe(mockGroupedJobsResponse);
+  });
 });
